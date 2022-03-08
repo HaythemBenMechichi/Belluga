50c17d8efe4165ca1697625088f89cc8636b5ed3
hassen <hassen.ahmadi@esprit.tn>
2022-03-08 23:11:49 +0100

    login last
    last

--- FirstTry/src/Controller/AdminController.php
+++ FirstTry/src/Controller/AdminController.php
@@ -3,18 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use phpDocumentor\Reflection\DocBlock\Tags\Uses;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Knp\Component\Pager\PaginatorInterface;
+/**
+ * @Route("/admin", name="admin_")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="app_admin")
+     * @Route("/users", name="users")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository,Request $request, PaginatorInterface $paginator): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        $donnees = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        $users = $paginator->paginate(
+            $donnees, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            3// Nombre de résultats par page
+        );
+
+        return $this->render('user/table.html.twig', [
+            'users' => $users,
         ]);
+
     }
 }
